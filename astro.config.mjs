94cbb8dcbbed861ea0beaf465f7bd3c1ffd722f7
twil3akine <twil3akine@gmail.com>
2025-07-19 18:51:25 +0900

    fix: cssが効いていなかったのを修正
    - src/styles -> public/ に移動
    - cssを直接importしていたのを、layouts/*.astroを作成し、そこでimportするように変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
+    site: 'https://twil3kaine.pages.dev',
     integrations: [
         starlight({
             title: "Twil3akine",
